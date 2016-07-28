013259574c7b8ae6aa96689468349deea5c34937
Zach Arnold <me@zacharnold.org>
2016-07-28 15:40:33 -0700

    smooth scrolling

--- dist/js/crucial-alliance.js
+++ dist/js/crucial-alliance.js
@@ -2385,3 +2385,18 @@ $(window).scroll(function() {
         $('.navbar').addClass('page-top');
     }
 });
+
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
--- src/js/custom.js
+++ src/js/custom.js
@@ -8,3 +8,18 @@ $(window).scroll(function() {
         $('.navbar').addClass('page-top');
     }
 });
+
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
